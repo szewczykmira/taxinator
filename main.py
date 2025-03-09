36b42604c99b8388e86fc34d3022bd8d06341cc2
Mira <6984271+szewczykmira@users.noreply.github.com>
2025-03-09 17:29:26 +0100

    Fix main file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
 from taxinator.convert_data import generate_converted_values
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     generate_converted_values()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
