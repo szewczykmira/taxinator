adc32d24d825cf5baa0dbfd53470a7424b8ec0ef
Mira <6984271+szewczykmira@users.noreply.github.com>
2025-03-09 18:05:35 +0100

    Handle one currency at a time

--- taxinator/currencybeacon.py
+++ taxinator/currencybeacon.py
@@ -4,31 +4,21 @@ from urllib.parse import urlencode, urljoin
 
 CURRENCYBEACON_URL = "https://api.currencybeacon.com/v1/"
 
-#
+
 # {
 #     "base": "EUR",
 #     "date": "2024-03-08",
 #     "meta": {
 #         "code": 200,
-#         "disclaimer": "Usage subject to terms: https://currencybeacon.com/terms"
+#         "disclaimer": "Usage subject to terms: https://currencybeacon.com/terms",
 #     },
-#     "rates": {
-#         "PLN": 4.29918938
-#     },
-#     "response": {
-#         "base": "EUR",
-#         "date": "2024-03-08",
-#         "rates": {
-#             "PLN": 4.29918938
-#         }
-#     }
+#     "rates": {"PLN": 4.29918938},
+#     "response": {"base": "EUR", "date": "2024-03-08", "rates": {"PLN": 4.29918938}},
 # }
 #
-#
-#
 
 
-def fetch_historical_rate(date: str, base: str, to_currencies: list[str]):
+def fetch_historical_rate(date: str, base: str, to_currency: str):
     if os.environ.get("CURRENCY_BEACON_API_KEY") is None:
         raise ValueError("Currencybeacon is not configured.")
 
@@ -36,10 +26,10 @@ def fetch_historical_rate(date: str, base: str, to_currencies: list[str]):
         {
             "date": date,
             "base": base,
-            "symbols": ",".join(to_currencies),
+            "symbols": to_currency,
             "api_key": os.environ.get("CURRENCY_BEACON_API_KEY"),
         }
     )
     url = urljoin(CURRENCYBEACON_URL, "historical") + f"?{params}"
     response = requests.get(url)
-    print(response.json())
+    return response.json()
